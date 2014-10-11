cdc281bda639e575f7fb80d5f88b6e9a1ca72213
rich-hart <richhohart@gmail.com>
2014-10-10 21:10:37 -0400

    Addind Wheelchair driving test script

---
+++ wheelchair_test.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
+
+time.sleep(.5)
+GPIO.output(4, GPIO.HIGH)
+time.sleep(.5)
+GPIO.output(4, GPIO.LOW)
+time.sleep(.5)
+GPIO.output(17, GPIO.HIGH)
+time.sleep(.5)
+GPIO.output(17, GPIO.LOW)
