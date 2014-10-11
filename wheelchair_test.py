db7599b199a78247193945c98eba3f65b3ff3fa6
rich-hart <richhohart@gmail.com>
2014-10-10 21:28:41 -0400

    fixing wheelchair pins

--- wheelchair_test.py
+++ wheelchair_test.py
@@ -1,30 +1,30 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
+
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(5, GPIO.OUT, initial=GPIO.LOW)
 
-'''
-
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)
-
-GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(6, GPIO.OUT, initial=GPIO.LOW)
 
 time.sleep(.5)
 
-GPIO.output(4, GPIO.HIGH)
+GPIO.output(5, GPIO.HIGH)
 
 time.sleep(.5)
 
-GPIO.output(4, GPIO.LOW)
+GPIO.output(5, GPIO.LOW)
 
 time.sleep(.5)
 
-GPIO.output(17, GPIO.HIGH)
+GPIO.output(6, GPIO.HIGH)
 
 time.sleep(.5)
 
-GPIO.output(17, GPIO.LOW)
-'''
+GPIO.output(6, GPIO.LOW)
+
+GPIO.cleanup()
+
