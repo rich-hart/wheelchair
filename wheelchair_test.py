fa728393f0a9c327d58d362bcb99df8557f12da4
rich-hart <richhohart@gmail.com>
2014-10-10 22:33:46 -0400

    checking all channels

--- wheelchair_test.py
+++ wheelchair_test.py
@@ -1,30 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 
+
+
+def Check_Channel(channel):
+    time.sleep(.5)
+
+    GPIO.output(channel, GPIO.HIGH)
+
+    time.sleep(.5)
+
+    GPIO.output(channel, GPIO.LOW)
+    
 FORDWARD_CHANNEL = 5
 REVERSE_CHANNEL = 13
+LEFT_CHANNEL = 19
+RIGHT_CHANNEL = 26
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(FORDWARD_CHANNEL, GPIO.OUT, initial=GPIO.LOW)
-
 GPIO.setup(REVERSE_CHANNEL, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(LEFT_CHANNEL, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(RIGHT_CHANNEL, GPIO.OUT, initial=GPIO.LOW)
 
-time.sleep(.5)
-
-GPIO.output(FORDWARD_CHANNEL, GPIO.HIGH)
-
-time.sleep(.5)
-
-GPIO.output(FORDWARD_CHANNEL, GPIO.LOW)
-
-time.sleep(3)
-
-GPIO.output(REVERSE_CHANNEL, GPIO.HIGH)
-
-time.sleep(.5)
-
-GPIO.output(REVERSE_CHANNEL, GPIO.LOW)
+Check_Channel(FORDWARD_CHANNEL)
+Check_Channel(REVERSE_CHANNEL)
+Check_Channel(LEFT_CHANNEL)
+Check_Channel(RIGHT_CHANNEL)
 
 GPIO.cleanup()
 
