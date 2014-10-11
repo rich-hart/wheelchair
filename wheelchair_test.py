35def1132362270b76403994f94ecd860308fc8d
rich-hart <richhohart@gmail.com>
2014-10-10 21:18:54 -0400

    adding in pin configuration

--- wheelchair_test.py
+++ wheelchair_test.py
@@ -1,14 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
+GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(4, GPIO.OUT, initial=GPIO.LOW)
+
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
 
 time.sleep(.5)
+
 GPIO.output(4, GPIO.HIGH)
+
 time.sleep(.5)
+
 GPIO.output(4, GPIO.LOW)
+
 time.sleep(.5)
+
 GPIO.output(17, GPIO.HIGH)
+
 time.sleep(.5)
+
 GPIO.output(17, GPIO.LOW)
